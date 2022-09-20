87d8a97a92345d47b50e10a1124f3f1db3c39b52
jjcorcis <0236835@up.edu.mx>
2022-09-19 22:59:32 -0500

    Examen parcial 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
+import React from 'react';
+import {useState} from 'react';
 import './App.css';
 
-function App() {
+export default function App() {
+  const[name ,setName]=useState('');
+  const[lastname ,setLastname]=useState('');
+  const[alias ,setAlias]=useState('');
+  
+  const isDisabledApellido = lastname!== undefined && lastname !== "";
+
+  const isDisabledApodo = alias!== undefined && alias !== "";
+
+  const isDisabledName = name!== undefined && name !== "";
+
+  
+const isDisableGlobal = isDisabledName && isDisabledApellido && isDisabledApodo;
+
+  const limpiar =()=>{
+
+    setName('');
+    setLastname('');
+    setAlias('');
+    setFormEnv(false);
+  }
+  const changeOfName=(event)=>{
+    setName(event.target.value)
+  }
+  const changeOfLastname=(event)=>{
+    setLastname(event.target.value)
+  }
+  const changeOfAlias=(event)=>{
+    setAlias(event.target.value)
+  }
+  const enviar = (event) => {
+    event.preventDefault();
+    setFormEnv(true);
+  }
+  
+  const [formularioEnv, setFormEnv] = useState(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      
+    <div className ='App'>
+      <p>Porfavor introduce tu nombre:</p>
+
+      <input onChange={changeOfName} value={name}/>
+   
+      <p>Por favor introduce tu Apellido:</p>
+      <input onChange={changeOfLastname} value={lastname}/>
+      <p>Introduce el Alias con el que te indentifican:</p>
+      <input onChange={changeOfAlias} value={alias}/>
+      
+      <br/>
+      <br/>
+      <br/>
+      <button disabled = {!isDisableGlobal} onClick={enviar} >Enviar</button>
+      <button onClick = {limpiar}>Limpiar</button>
+      <br/>
+      <br/>
+      <br/>
+      <p>Tus datos introducidos fueron: </p>
+      {formularioEnv && (<p> {`${name}`} {`${lastname}`} {`${alias}`}</p>)}
     </div>
+
+
   );
+  
 }
 
-export default App;
